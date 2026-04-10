e1abd6378eb09270058510e22015d40c32d6f485
Hikl845 <freelancer198756@gmail.com>
2026-04-10 05:03:49 +0300

    Update NoteRepository.java

--- src/main/java/com/example/homeworkmodule15/NoteRepository.java
+++ src/main/java/com/example/homeworkmodule15/NoteRepository.java
@@ -1,6 +1,6 @@
-package com.example.homeworkmodule15;
+package com.example.homeworkmodule16;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.homeworkmodule15.entity.Note;
+import com.example.homeworkmodule16.entity.Note;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
 }
